0d7052bfa8fc26937cf47f7f7f72df2039c5325b
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-08-01 11:45:56 -0300

    ex1

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Field from './Field'
 
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+
+const reducers = combineReducers({
+   field: () => ({value: 'Opa'}) 
+})
 
 ReactDOM.render(
-   <Field initialValue="Texto" />
+   <Provider store={createStore(reducers)} >
+      <Field initialValue="Texto" />
+   </Provider>
    , document.querySelector('.app')
 )
 
